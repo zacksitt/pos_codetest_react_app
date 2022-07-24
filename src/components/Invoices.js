83c1fc6d65f135b321c3ac7301f2ed7a1194637e
zacksitt <myoothu.jva@gmail.com>
2022-07-25 01:49:53 +0630

    update ui

--- src/components/Invoices.js
+++ src/components/Invoices.js
@@ -1,33 +1,41 @@
 import { Outlet, Link } from "react-router-dom";
 import React,{useRef,useState,useEffect} from 'react';
-import axios from "axios";
-import Article from "./Article";
 import { useSelector, useDispatch } from 'react-redux'
+import CanvasJSReact from './canvasjs.react';
+
+
 import {
+    
   fetch,
   selectAllInvoices,
-  add,
-  update,
   fetchInvoices,
   addNewInvoice,
   getProducts,
-  setProducts
+  getTotalCount,
+  fetchChartData
 } from '../reducer/invoices/invoicesSlice'
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const InvoiceRow = ({ invoice }) => {
+
     return (
         <tr>
             <th scope="row">{invoice.id}</th>
             <td>{invoice.customer_name}</td>
             <td>{invoice.saleperson_name}</td>
             <td>{invoice.date}</td>
-            <td> 
+            <td>{invoice.total_amount}</td>
+            {/* <td> 
                 {
-                    invoice.myproducts.map((myproduct,index) => (
-                        <span>{myproduct.name}</span>
+                    invoice.products.map((myproduct,index) => (
+                        <span> 
+                            {index > 0 ? ', ':''}
+                            {myproduct.name}
+                        </span>
                     ))
                 }
-            </td>
+            </td> */}
         </tr>
 
     )
@@ -38,19 +46,33 @@ export const Invoices = () => {
     const [customer_name, setCustomerName] = useState('')
     const [saleperson_name, setSalepersonName] = useState('')
     const [date, setDate] = useState('')
-    const [produc_name,setProductName] = useState('')
-    const [products,setProducts] = useState([]);
-    const [myproducts,setMyProducts] = useState([]);
+    const [produc_name, setProductName] = useState('')
+    const [products, setProducts] = useState([]);
+    const [myproducts, setMyProducts] = useState([]);
+    const [myproduct_warning,setMyProductWarning] = useState('');
+    const [total_amount,setTotalAmount] = useState(0);
+    const [timerange,setTimeRange] = useState("daily");
+    const [options,setOptions] = useState({})
+    const [pagenum,setPageNum] = useState(1);
 
     const onCustomerChange = (e) => setCustomerName(e.target.value)
     const onSalePersonChange = (e) => setSalepersonName(e.target.value)
     const onDateChange = (e) => setDate(e.target.value)
     let allproducts = useSelector(getProducts)
 
+    const calTotalAmount = () => {
+
+        let total_amount = 0;
+        for (const mproduct of myproducts) {
+            total_amount = total_amount + mproduct.price;
+        }
+        setTotalAmount(total_amount);
+    }
+
     const onProductNameChange = (e) => {
         
+        setMyProductWarning('')
         let noProducts = [];
-
         for (const p of allproducts) {
             if(p.name.includes(e.target.value)){
                 noProducts.push(p);
@@ -60,25 +82,104 @@ export const Invoices = () => {
         setProductName(e.target.value);
     }
 
+    const removeMyProduct = async (index) => {
+
+        console.log("removeMyProduct",index);
+        let newMyProducts = [];
+        if(myproducts.length > 1)
+            myproducts.splice(index, 1); 
+        console.log("myproducts",products);
+
+        setMyProducts(myproducts);
+        calTotalAmount();
+    }
     
     const invoices = useSelector(selectAllInvoices)
+    const totalCount = useSelector(getTotalCount)
+
+    //const chartData = useSelector(selectChartData)
+
+    //const allmyproducts = useSelector(selectAllInvoices)
     const dispatch = useDispatch()
 
-    useEffect(() => {
-          dispatch(fetchInvoices())
-      }, [dispatch])
+    useEffect(async () => {
 
-    console.log("fetch invoices",invoices);
+          dispatch(fetchInvoices(pagenum))
+          let chartData = await dispatch(fetchChartData(timerange)).unwrap()
+          createChartOption(chartData,timerange);
+
+      }, [dispatch])
+    
+    
+    const generateDataPoints = (cdata,timerange) => {
+        
+        console.log("generate",timerange);
+        let xValue = {};
+        let data = [];
+		for (const [index,i] of cdata.entries()) {
+            if(timerange == "daily"){
+                data.push({x:new Date(i._id),y:i.count})
+                xValue.valueFormatString = "YYYY-MM-DD" 
+                xValue.title = "Daily";
+
+            }else if(timerange == "weekly"){
+
+                data.push({x:index,y:i.count})
+                xValue.prefix = "W";
+                xValue.interval = 2;
+                xValue.title = "Weekly";
+            }else{
+
+                data.push({x:new Date(i.createdDate),y:i.count})
+                xValue.valueFormatString = "MMM" 
+                xValue.title = "Monthly";
+            }
+                
+		}
+        console.log("data",data);
+		return {xValue:xValue,dataPoints:data};
+	}
+
+    const createChartOption = (chartdata,timerange) => {
+
+        console.log("chartdata",chartdata);
+        let data = generateDataPoints(chartdata,timerange);
+        console.log("data",data);
+
+        let options = {
+            theme: "light2", // "light1", "dark1", "dark2"
+            animationEnabled: true,
+            zoomEnabled: true,
+            title: {
+                text: "Revenue Charts"
+            },
+            axisX: data.xValue,
+            
+            data: [{
+                type: "area",
+                dataPoints: data.dataPoints
+            }]
+        }
+        setOptions(options);
+    }
+   
+   
+
     let content = invoices.map((i) => (
         <InvoiceRow key={i.id} invoice={i} />
       ))
     
     const onChoose = async (myproduct) => {
+
             try {
 
+                console.log("onChoose");
+                setProductName('')
                 myproducts.push(myproduct);
                 setMyProducts(myproducts);
-                console.log("my products",myproducts);
+                setProducts([])
+
+                calTotalAmount();
 
             } catch (err) {
                     console.error('Failed to save the post: ', err)
@@ -87,14 +188,21 @@ export const Invoices = () => {
         }
     }
 
-    const onSavePostClicked = async () => {
-        console.log("on click");
+    const onSavePostClicked = async (e) => {
+        e.preventDefault()    
         try {
+                if(myproducts.length == 0){
 
-                await dispatch(addNewInvoice({ customer_name, saleperson_name, date,myproducts})).unwrap()
-                setCustomerName('')
-                setSalepersonName('')
-                setDate('')
+                    setMyProductWarning('Pleae add product.');
+                }else{
+
+                    await dispatch(addNewInvoice({ customer_name, saleperson_name, date,products:myproducts,total_amount})).unwrap()
+                    setCustomerName('')
+                    setSalepersonName('')
+                    setDate('')
+                    setMyProducts([])
+                }
+                
         
         } catch (err) {
                 console.error('Failed to save the post: ', err)
@@ -102,62 +210,132 @@ export const Invoices = () => {
 
         }
     }
+
+    const onClickTimeRange = async (timerange) => {
+
+        console.log("timerange",timerange);
+        setTimeRange(timerange);
+        let dd = await dispatch(fetchChartData(timerange)).unwrap()
+        createChartOption(dd,timerange);
+    }
+
+    const onClickNext = async () => {
+
+        let pnum = pagenum + 1;
+        setPageNum(pnum);
+        await dispatch(fetchInvoices(pnum)).unwrap();
+
+    }
+
+    const onClickPrivious = async () => {
+
+        let pnum = pagenum - 1;
+        setPageNum(pnum);
+        await dispatch(fetchInvoices(pnum)).unwrap();
+    }
     
     return (
         <div class="invoices mt-3">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
+
                     <div className="content-head mb-3">
-                        <h2>Invoices </h2>
+                        <h3>Invoices </h3>
                         {/* <a className="btn btn-success btn-lg" onClick={() => dispatch(add())}> + Add New </a> */}
                     </div>
                     <hr></hr>
-                    <div className="content-form">
-                    <form>
-                        <div class="form-group row">
-                            <label for="staticEmail" class="col-sm-2 col-form-label">Customer name</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" placeholder="customer name"  onChange={onCustomerChange}/>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Sale person name</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" placeholder="sale person name"  onChange={onSalePersonChange}/>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Date</label>
-                            <div class="col-sm-10">
-                            <input type="text" class="form-control" id="inputPassword" placeholder="date"  onChange={onDateChange}/>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="inputPassword" class="col-sm-2 col-form-label">Products</label>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputPassword" placeholder="products"  onChange={onProductNameChange}/>
-                                <div>
-                                <div className="autocomplete-items">
-                                    <ul className="list-group">
-                                    { products && products.map((product, index) => (
-                                        <li key={index}>
-                                            <a className="list-group-item text-dark" onClick={() => onChoose(product)} href="#">{product.name}</a>
-                                        </li>
-                                    ))}
-                                    </ul>
+                    <div className="row">
+                  
+                    <div className="invoice-form col-md-7">
+                        <div className="content-form mt-3">
+                        <form onSubmit={onSavePostClicked} method="post" >
+                            <div class="form-group row">
+                                <label for="staticEmail" class="col-sm-3 col-form-label">Customer name</label>
+                                <div class="col-sm-7">
+                                    <input type="text" class="form-control" id="inputPassword"  value={customer_name} placeholder="customer name"  onChange={onCustomerChange} required/>
                                 </div>
                             </div>
+                            <div class="form-group row">
+                                <label for="inputPassword" class="col-sm-3 col-form-label">Sale person name</label>
+                                <div class="col-sm-7">
+                                <input type="text" class="form-control" id="inputPassword"  value={saleperson_name} placeholder="sale person name"  onChange={onSalePersonChange} required/>
+                                </div>
                             </div>
-                            
+                            <div class="form-group row">
+                                <label for="inputPassword" class="col-sm-3 col-form-label">Date</label>
+                                <div class="col-sm-7">
+                                <input type="date" class="form-control" id="inputPassword"  value={date} placeholder="date"  onChange={onDateChange} required/>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="inputPassword" class="col-sm-3 col-form-label">Products</label>
+                                <div class="col-sm-7">
+                                    <input type="text" class="form-control" id="inputPassword"  value={produc_name} placeholder="products"  onChange={onProductNameChange}/>
+                                
+                                    <div>
+                                        <div className="autocomplete-items">
+                                            <ul className="list-group">
+                                            { products && products.map((product, index) => (
+                                                <li key={index}>
+                                                    <a className="list-group-item text-dark" onClick={() => onChoose(product)} href="#">{product.name}</a>
+                                                </li>
+                                            ))}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    {
+                                        myproduct_warning != "" ? (
+                                            <label className="text-danger text-sm">{myproduct_warning}</label>
+                                        ):(
+                                            ''
+                                        )
+                                    }
+                                    
+                                </div>
+                                <div className="col-sm-10">
+                                    <div className="product-box row">
+                                        { myproducts && myproducts.map((myproduct,index) => (
+                                            <div className="col-sm-4">
+                                                <div class="card">
+                                                    <img class="card-img-top" src={myproduct.image} alt="Card image cap"/>
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">{myproduct.name}</h5>
+                                                        <p class="card-text text-secondary">
+                                                            ${myproduct.price} | {myproduct.stock} in stock</p>
+                                                        <a href="#" class="btn btn-primary" onClick={() => removeMyProduct(index)} >Remove</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))    
+                                        }
+                                    </div>           
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <button type="submit" className="btn btn-success btn-lg" ><i className="fa fa-save"></i> Save</button>
+                            </div>
+                            </form>
                         </div>
-                        <div class="form-group row">
-                            <a className="btn btn-success btn-lg text-light" onClick={onSavePostClicked}><i className="save"></i> Save</a>
-                        </div>
-                        </form>
                     </div>
+                    <div className="col-md-5">
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-secondary"  onClick={() => onClickTimeRange("daily")} >Daily</button>
+                            <button type="button" class="btn btn-secondary" onClick={() => onClickTimeRange("weekly")} >Weekly</button>
+                            <button type="button" class="btn btn-secondary" onClick={() => onClickTimeRange("yearly")} >Monthly</button>
+                        </div>
+                            <CanvasJSChart options = {options} 
+                                    /* onRef={ref => this.chart = ref} */
+                                />
+                    </div>
+                    </div>
+                   
 
                     <hr></hr>
+                    <div class="btn-group mb-3" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-primary" disabled={pagenum === 1} onClick={() => onClickPrivious()} ><i className="fa fa-arrow-left"></i> Privious</button>
+                            <button type="button" class="btn btn-primary"   disabled={totalCount <= 10 * pagenum}  onClick={() => onClickNext()} >Next <i className="fa fa-arrow-right"></i> </button>
+                        </div>
                     <div className="content-body">
                         <table class="table">
                             <thead>
@@ -166,7 +344,7 @@ export const Invoices = () => {
                                     <th scope="col">Customer name</th>
                                     <th scope="col">Sale person name</th>
                                     <th scope="col">Sale date</th>
-                                    <th scope="col">Products</th>
+                                    <th scope="col">Total Amount</th>
                                 </tr>
                             </thead>
                             <tbody>
--- src/reducer/invoices/invoicesSlice.js
+++ src/reducer/invoices/invoicesSlice.js
@@ -1,9 +1,13 @@
 import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
+import axios from "axios";
 
 export const invoicesSlice = createSlice({
   name: 'invoices',
   initialState: {
-    invoices:[]
+    invoices:[],
+    myproducts:[],
+    chartdata:[],
+    totalCount:0,
   },
   reducers: {
     fetch: state => {
@@ -29,31 +33,73 @@ export const invoicesSlice = createSlice({
       .addCase(fetchInvoices.fulfilled, (state, action) => {
        // state.status = 'succeeded'
         // Add any fetched posts to the array
-        console.log("fullfilled");
-        state.invoices = state.invoices.concat(action.payload)
+        console.log("fullfilled",action.payload);
+        state.invoices = [];
+        state.invoices = state.invoices.concat(action.payload.data)
+        state.totalCount = action.payload.totalCount;
       })
+       
       .addCase(addNewInvoice.fulfilled, (state, action) => {
-        state.invoices.push(action.payload)
+        state.invoices.unshift(action.payload)
+      })
+      .addCase(fetchChartData.fulfilled, (state, action) => {
+        state.chartdata = action.payload
       })
     
   },
 })
 
+export const getTotalCount = (state) => state.invoices.totalCount
 export const selectAllInvoices = (state) => state.invoices.invoices
+export const selectAllMyproducts = (state) => state.invoices.myproducts
+export const selectChartData = (state) => state.invoices.chartdata
 export const getProducts = (state) => {
-    return [{"name":"IPhone"},{"name":"Samsung"},{"name":"One Plus"},{"name":"Xaimi"}];
+    return [
+        {"name":"iPhone 13 Pro","image":"https://www.apple.com/v/iphone-13-pro/g/images/key-features/compare/compare_iphone_13_pro__ezrebuldmju6_large.jpg","price":999,"stock":2},
+        {"name":"iPhone 13","image":"https://www.apple.com/v/iphone-13-pro/g/images/key-features/compare/compare_iphone_13_pro__ezrebuldmju6_large.jpg","price":699,"stock":24},
+        {"name":"iPhone SE","image":"https://www.apple.com/v/iphone-13-pro/g/images/key-features/compare/compare_iphone_se__fifp7zy8jlaq_large.jpg","price":429,"stock":55},
+        {"name":"iPhone 12","image":"https://www.apple.com/v/iphone-13-pro/g/images/key-features/compare/compare_iphone_12__fk7mpufyl1u2_large.jpg","price":599,"stock":100}
+    ];
 }
 export const setProducts = (products) => {
     console.log("setProducts",products);
     return products
 }
 
-export const fetchInvoices = createAsyncThunk('invoices/fetInvoices', async () => {
+export const fetchInvoices = createAsyncThunk('invoices/fetInvoices', async (pagenum) => {
 
-    //const response = await client.get('/fakeApi/posts')
-    //return response.data
-    console.log("fetchInvoices");
-    return [{id:1,customer_name:"Aung Aung",saleperson_name:"Su Zar",date:"22/07/2022",myproducts:[{name:"IPhone"}]}];
+    let headers;
+    headers = {
+
+    'Content-Type': 'application/json',
+    'Authorization': process.env.REACT_APP_API_TOKEN
+
+    }
+    let param = {
+      count:10,
+      pagenum:pagenum
+    }
+    let res = await axios.post(process.env.REACT_APP_API_URL + "/auth/invoices",param,{headers:headers});
+    return res.data;
+
+   // return [{id:1,customer_name:"Aung Aung",saleperson_name:"Su Zar",date:"22/07/2022",myproducts:[{name:"IPhone"}]}];
+})
+
+export const fetchChartData = createAsyncThunk('invoices/fetchChartData', async (timerange) => {
+
+  let headers;
+  headers = {
+
+  'Content-Type': 'application/json',
+  'Authorization': process.env.REACT_APP_API_TOKEN
+
+  }
+  let param = {
+    timerange
+  }
+  let res = await axios.post(process.env.REACT_APP_API_URL + "/auth/invoice/report",param,{headers:headers});
+  return res.data.data;
+ // return [{id:1,customer_name:"Aung Aung",saleperson_name:"Su Zar",date:"22/07/2022",myproducts:[{name:"IPhone"}]}];
 })
 
 export const addNewInvoice = createAsyncThunk(
@@ -63,7 +109,20 @@ export const addNewInvoice = createAsyncThunk(
         console.log("new invoice",initialInvoice);
         //const response = await client.post('/fakeApi/posts', initialPost)
         //return response.data
-        return initialInvoice
+        //return initialInvoice
+        let headers;
+        headers = {
+
+        'Content-Type': 'application/json',
+        'Authorization': process.env.REACT_APP_API_TOKEN
+
+        }
+        let param = {
+          count:5,
+          pagenum:1
+        }
+        let res = await axios.post(process.env.REACT_APP_API_URL + "/auth/invoice",initialInvoice,{headers:headers});
+        return res.data.data;
     }
   )
 
